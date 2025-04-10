5967334c12a7c6dd417ec352163d6441f862a24c
Andrew Thow <thow1976@hotmail.co.uk>
2025-04-10 20:33:57 +0100

    1.8 DI(Hilt): - core hilt implemented - AppModule created - MainActivity updated - AndroidManifest updated - SribbleDashApplication class created

--- app/src/main/java/com/example/scribbledash/MainActivity.kt
+++ app/src/main/java/com/example/scribbledash/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.scribbledash.navigation.NavigationRoot
 import com.example.scribbledash.features.theme.ScribbleDashTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/scribbledash/ScribbleDashApplication.kt
@@ -0,0 +1,7 @@
+package com.example.scribbledash
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ScribbleDashApplication : Application()
