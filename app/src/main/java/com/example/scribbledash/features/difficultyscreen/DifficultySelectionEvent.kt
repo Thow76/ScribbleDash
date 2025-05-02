c09139d98ebd7b93daba55350ee4c997c2d062d5
Andrew Thow <thow1976@hotmail.co.uk>
2025-05-02 20:36:41 +0100

    1.24 UI(DifficultyScreen): DifficultyScreen refactored

--- app/src/main/java/com/example/scribbledash/features/difficultyscreen/DifficultySelectionEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scribbledash.features.difficultyscreen
-
-sealed class DifficultySelectionEvent {
-    object Close : DifficultySelectionEvent()
-    data class SelectDifficulty(val difficulty: Difficulty) : DifficultySelectionEvent()
-}
--- app/src/main/java/com/example/scribbledash/features/difficultyscreen/DifficultySelectionUiState.kt
+++
@@ -1,5 +0,0 @@
-package com.example.scribbledash.features.difficultyscreen
-
-data class DifficultySelectionUiState(
-    val options: List<Difficulty> = Difficulty.values().toList()
-)
